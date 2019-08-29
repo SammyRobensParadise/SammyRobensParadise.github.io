b214a13697422412ab98f2e8fcf3d9393c91d787
Sammy Robens-Paradise <srobensparadise@gmail.com>
2019-08-29 00:05:52 -0700

    begain  implementing animation for bc-ferries-webpage

--- bc-ferries.js
+++ bc-ferries.js
@@ -8,5 +8,17 @@
 window.addEventListener("load", setBCFerriesVars);
 
 function setBCFerriesVars(){
-    window.timeline_handler = new ViewHandler()
+    timelineManager  = new bcFerriesAnimationHandler('timeline-outer','timeline','timeline',{
+        animClass1 = "timeline-anim",
+        animClass2 = "timeline-elems-anim"
+    })
+}
+class bcFerriesAnimationHandler{
+    constructor(el,target_el,destination_el,class_data){
+        this.elem = document.getElementById(el);
+        this.target_elem  = document.getElementById(target_el);
+        this.destination_elem = document.getElementById(destination_el);
+        this.class_obj = class_data;
+    }
+
 }
