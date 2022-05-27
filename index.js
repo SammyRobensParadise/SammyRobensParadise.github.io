d4e17dd1e68e2e151d902ec56b19d2849d158a88
Sammy Robens-Paradise <srobensparadise@gmail.com>
2022-05-26 23:49:54 -0400

    f

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
 function searchText(value) {
-  debugger;
+  const listItems = Array.from(document.getElementById('wiki-list').children);
+  if (value === '') {
+    listItems.forEach(function (listItem) {
+      listItem.style.display = 'list-item';
+    });
+  } else {
+    listItems.forEach(function (listItem) {
+      const hasText = listItem.innerHTML.toLowerCase().indexOf(value.toLowerCase()) !== -1;
+      if (!hasText) {
+        listItem.style.display = 'none';
+      } else {
+        listItem.style.display = 'list-item';
+      }
+    });
+  }
+}
+
+function clearSearch() {
+  document.getElementById('wiki-search-input').value = '';
+  const listItems = Array.from(document.getElementById('wiki-list').children);
+  listItems.forEach(function (listItem) {
+    listItem.style.display = 'list-item';
+  });
 }
